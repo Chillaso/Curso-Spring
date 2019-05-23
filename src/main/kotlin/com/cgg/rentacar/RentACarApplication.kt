8a57ef3f525906560a59d5c4e98fe6ff2a16ee99
Carlos García González <cgarcia.gonzalez@atsistemas.com>
2019-05-23 19:04:36 +0200

    First commit

---
+++ src/main/kotlin/com/cgg/rentacar/RentACarApplication.kt
@@ -0,0 +1,11 @@
+package com.cgg.rentacar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RentACarApplication
+
+fun main(args: Array<String>) {
+	runApplication<RentACarApplication>(*args)
+}
