4260692f177043adcabcf69129f7d75f586f382a
Carlos García González <cgarcia.gonzalez@atsistemas.com>
2019-05-26 19:57:06 +0200

    CarMapper devuelve un objeto vacio

--- src/main/kotlin/com/cgg/rentacar/service/mapper/CarMapperServiceImpl.kt
+++ src/main/kotlin/com/cgg/rentacar/service/mapper/CarMapperServiceImpl.kt
@@ -1,4 +1,10 @@
 package com.cgg.rentacar.service.mapper
 
-class CarMapperServiceImpl {
+import com.cgg.rentacar.model.dto.CarDto
+import com.cgg.rentacar.model.entity.CarEntity
+import com.cgg.rentacar.service.MapperService
+
+class CarMapperServiceImpl : MapperService<CarDto, CarEntity>{
+
+    override fun map(carDto: CarDto): CarEntity = CarEntity()
 }
