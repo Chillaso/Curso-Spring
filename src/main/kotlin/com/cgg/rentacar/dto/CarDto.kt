a55ddf5ea4b49ed9df31cc03186d3ca56cb68791
Carlos García González <cgarcia.gonzalez@atsistemas.com>
2019-06-02 21:26:36 +0200

    Añadida validacion, past or present para el año de registro de un coche

--- src/main/kotlin/com/cgg/rentacar/dto/CarDto.kt
+++ src/main/kotlin/com/cgg/rentacar/dto/CarDto.kt
@@ -1,10 +1,11 @@
 package com.cgg.rentacar.dto
 
 import javax.validation.constraints.NotNull
+import javax.validation.constraints.PastOrPresent
 import javax.validation.constraints.Pattern
 
 data class CarDto(
         val idCar: Int,
         @NotNull val carPlate: String,
-        @NotNull @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$")val registrationYear: String,
+        @NotNull @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$") @PastOrPresent val registrationYear: String,
         @NotNull val model: String)
